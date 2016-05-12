349770ac802aa19917b13e3527afefa202ebba73
Mike Mathew <m2mathew@me.com>
2016-05-12 10:05:40 -0500

    toLowerCase() in process

--- string-methods/str.toLowerCase.js
+++ string-methods/str.toLowerCase.js
@@ -5,4 +5,28 @@
  *
  */
 
+function jsToLowerCase (string) {
+	var arrayToReturn = [];
 
+	// for loop to see what the character is at each index
+	for (var i = 0; i < string.length; i++) {
+		var thisChar = string.charAt(i);
+
+		// if the character is not whitespace, then push it to the array
+		if (thisChar >= 'A' && thisChar <= 'Z') {
+			// need to deal with the specific unicode for each character
+			// look for pattern to change to lower case through numbers
+			// arrayToReturn.push(thisChar);
+		}
+	}
+
+	var stringToReturn = arrayToReturn.join('');
+
+	console.log(stringToReturn);
+	return stringToReturn;
+}
+
+jsToLowerCase('write');
+jsToLowerCase('EVERYTHING');
+jsToLowerCase('In');
+jsToLowerCase('JavaScript.');
